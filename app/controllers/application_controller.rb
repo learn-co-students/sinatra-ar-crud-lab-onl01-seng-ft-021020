afa609c4f41916baa1825075501109e610edfe28
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-03-30 18:34:23 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
-
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -7,7 +6,22 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-
+  
   get '/' do
+    redirect '/articles'
   end
+
+get '/articles'
+@articles = Article.all
+erb :index
+end
+
+
+
+  delete '/articles/:id' do
+    @post = Post.find(params["id"])
+    @post.destroy
+   redirect '/articles'
+  end
+  
 end
