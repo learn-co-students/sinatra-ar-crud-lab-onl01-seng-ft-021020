7239ac26eee356c3616156cb1d13fa255215c2f0
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-04-05 00:59:33 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,69 +1,125 @@
 require_relative '../../config/environment'
 require 'pry'
+
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-  
-    get '/' do
-    redirect to '/articles'
+
+  get '/' do
+
   end
-  
+
   get '/articles/new' do
-  erb :new
+    @article = Article.new
+   # binding.pry
+    erb :new
   end
 
- 
-  
   post '/articles' do
-  @article = Article.create(params)
-  # binding.pry
-  redirect to "/articles/#{@article.id}"
+    Article.create(params)
+    redirect '/articles'
   end
-  
-  
 
-
-get '/articles' do
-@articles = Article.all
-erb :index
-end
+  get '/articles' do
+    @article = Article.all
+    erb :index
+  end
 
   get '/articles/:id' do
-    # binding.pry
     @article = Article.find(params["id"])
-  erb :show
+    erb :show
   end
-get "/articles/:id/edit" do
-   @article = Article.find(params["id"])
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params["id"])
     erb :edit
   end
 
   patch '/articles/:id' do
-     binding.pry
+    binding.pry
+    id = params["id"]
     new_params = {}
-    old_article = Article.find(params["id"])
+    old_article = Article.find(id)
     new_params[:title] = params["title"]
     new_params[:content] = params["content"]
-    old_article.update(new_params)
-    redirect "/articles/#{params["id"]}"
-   
+    old_post.update(new_params)
+
+    redirect "/articles/#{id}"
   end
 
-    delete '/articles/:id' do
+  delete '/articles/:id/delete' do
     @article = Article.find(params["id"])
     @article.destroy
-   redirect '/articles'
+    erb :delete
   end
-  
-  
-  
-  
-  #get = gets
-  #patch = update
-  #post = 
-  
-  
 end
+# class ApplicationController < Sinatra::Base
+
+#   configure do
+#     set :public_folder, 'public'
+#     set :views, 'app/views'
+#   end
+  
+#     get '/' do
+#     redirect to '/articles'
+#   end
+  
+#   get '/articles/' do
+# @articles = Article.all
+# erb :index
+# end
+
+#   get '/articles/new' do
+    
+#   @article = Article.new
+#   # binding.pry
+#   erb :new
+#   end
+  
+#   post '/articles' do
+#     # binding.pry
+#   @article = Article.create(params)
+  
+#   redirect to "/articles/#{@article.id}"
+#   end
+
+
+#   get '/articles/:id' do
+#     # binding.pry
+#     @article = Article.find(params["id"])
+#   erb :show
+#   end
+# get "/articles/:id/edit" do
+#   @article = Article.find(params["id"])
+#     erb :edit
+#   end
+
+#   patch '/articles/:id' do
+#     binding.pry
+#     new_params = {}
+#     old_article = Article.find(params["id"])
+#     new_params[:title] = params["title"]
+#     new_params[:content] = params["content"]
+#     old_article.update(new_params)
+#     redirect "/articles/#{params["id"]}"
+   
+#   end
+
+#     delete '/articles/:id' do
+#     @article = Article.find(params["id"])
+#     @article.destroy
+#   redirect '/articles'
+#   end
+  
+  
+  
+  
+#   #get = gets
+#   #patch = update
+#   #post = 
+  
+  
+# end
